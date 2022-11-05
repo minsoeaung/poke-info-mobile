ed2805973682e86a20cf7273275b047bb143d976
minsoeaung <minsoeaung2001@gmail.com>
2022-11-05 14:04:40 +0630

    remove "expo-app-loading" with "expo-splash-screen"

--- App.tsx
+++ App.tsx
@@ -1,26 +1,43 @@
 import { DotGothic16_400Regular, useFonts } from '@expo-google-fonts/dotgothic16';
 import { NavigationContainer } from '@react-navigation/native';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { app } from './constants/colors';
 import { BottomTabsNavigator } from './screens/BottomTabsNavigator';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
     const [fontsLoaded] = useFonts({ DotGothic16_400Regular });
 
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
     if (!fontsLoaded) {
-        return <AppLoading />;
+        return null;
     }
 
     return (
         <SafeAreaProvider>
-            <NavigationContainer>
-                <BottomTabsNavigator />
-                <StatusBar style="light" backgroundColor={app.darkColor} />
-            </NavigationContainer>
+            <View style={styles.container} onLayout={onLayoutRootView}>
+                <NavigationContainer>
+                    <BottomTabsNavigator />
+                    <StatusBar style="light" backgroundColor={app.darkColor} />
+                </NavigationContainer>
+            </View>
         </SafeAreaProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
